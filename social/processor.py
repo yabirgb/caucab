7fb0975a87b27ce10adf90141a2c7e95fe4e4d16
Yábir Garcia <zanklord@gmail.com>
2016-03-01 19:43:04 +0100

    Fixed bug with processor and anonymous user

--- social/processor.py
+++ social/processor.py
@@ -1,7 +1,10 @@
 from .models import Profile, Circle
 
 def circles(request):
-    profile = Profile.objects.get(user = request.user)
-    circles = Circle.objects.all().filter(owner = profile)
+    if not request.user.is_anonymous():
+        profile = Profile.objects.get(user = request.user)
+        circles = Circle.objects.all().filter(owner = profile)
 
-    return {'circles': circles}
+        return {'circles': circles}
+    else:
+        return {}
